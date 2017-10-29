bc93888eeecc927b99a4861b7bf511036015bae8
Nick <nickrsan@bitbucket.org>
2017-10-29 21:30:13 +1300

    First crack at indexing for CTD data - will likely be an eventual performance issue.

--- icefish/models.py
+++ icefish/models.py
@@ -6,9 +6,9 @@ from django.db import models
 # Create your models here.
 
 class CTD(models.Model):
-	temp = models.FloatField()
-	pressure = models.FloatField()
-	conductivity = models.FloatField(blank=True, null=True)
-	datetime = models.DateTimeField()
+	temp = models.FloatField(db_index=True)
+	pressure = models.FloatField(db_index=True)
+	conductivity = models.FloatField(blank=True, null=True, db_index=True)
+	datetime = models.DateTimeField(db_index=True)
 	server_datetime = models.DateTimeField()  # the server reading the data's timestamp
 	measured = models.BooleanField(default=True)  # used as a flag if we interpolate any values. If measured == True, then it's direct off the CTD
