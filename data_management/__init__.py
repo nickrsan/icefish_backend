015e8835fbdfaceb81a898c683f89a25e1fd1c01
Nick Santos <nickrsan@users.noreply.github.com>
2017-10-30 20:21:45 +1300

    finishing adding the rest of the hydrophone audio start, hgignore updates

---
+++ data_management/__init__.py
@@ -0,0 +1,13 @@
+from icefish_backend import settings
+from icefish.models import HydrophoneAudio
+
+def hydrophone_pipeline():
+	# start by reading folder with incoming hydrophone data
+	# pass it through whatever generates spectrographs
+	# send each file to flac,
+	# delete original
+	#
+
+	# create HydrophoneAudio object for each
+	# end by saving it
+	pass
