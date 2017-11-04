2b2ffefb00aec56fc2ebb2419b8f468b29bb0916
Nick Santos <nickrsan@users.noreply.github.com>
2017-11-04 15:14:28 +1300

    More integrity checks in the FLAC processing pipeline - it checks if the wave file has already been loaded, if the FLAC file exists, and then checks the integrity of the FLAC file before deleting the WAV file. Also some docstrings

--- icefish/data_management/__init__.py
+++ icefish/data_management/__init__.py
@@ -8,7 +8,7 @@ import wave
 
 import arrow
 
-from icefish.models import HydrophoneAudio
+from icefish.models import HydrophoneAudio, FLACIntegrityError
 from icefish_backend import settings
 
 log = logging.getLogger("icefish.audio")
@@ -26,10 +26,15 @@ def hydrophone_pipeline(inbound_folder=settings.WAV_STORAGE_FOLDER, outbound_fol
 	wav_files = [wav for wav in new_files if wav.endswith(".wav")]  # filter to only files ending with the .wav extension
 
 	for wav in wav_files:
+		log.debug("Loading {}".format(wav))
 		# THIS SHOULD CHECK FOR A FILE WITH THE SAME OUTPUT NAME TO PROTECT AGAINST DUPLICATES - we could also use an index
-		audio = HydrophoneAudio()
 
 		full_input = os.path.join(inbound_folder, wav)
+		if already_loaded(full_input):  # check if that wav file path is already in the database. If so, skip it
+			log.debug("Skipping already loaded file at {}".format(full_input))
+			continue
+
+		audio = HydrophoneAudio()
 		base_name = os.path.basename(wav).split(".")[0]  # get just the root filename without directory or extension - directory won't be included here
 		audio.wav = full_input
 		full_output = os.path.join(outbound_folder, "{}.flac".format(base_name))
@@ -46,8 +51,21 @@ def hydrophone_pipeline(inbound_folder=settings.WAV_STORAGE_FOLDER, outbound_fol
 		audio.make_spectrogram(os.path.join(spectrogram_folder, "{}.png".format(base_name)))
 		audio.save()
 
-		# os.unlink(full_input)  # now remove the wav file
+		try:
+			audio.remove_wav()  # automatically checks the integrity of the FLAC file before deleting WAV file
+		except FLACIntegrityError:
+			log.warning("Unable to remove wav file {}. FLAC file is invalid. Regenerate the flac file for audio record with ID {}".format(audio.wav, audio.id))
 
-	# pass it through whatever generates spectrographs
-	# delete original
+def already_loaded(wave_path):
+	"""
+		Checks if the wave named at wave_path has already been loaded
+	:param wave_path:
+	:return:
+	"""
+	try:
+		HydrophoneAudio.objects.get(wav=wave_path)
+	except HydrophoneAudio.DoesNotExist:
+		return False
 
+	# If succeeds without error, then it's loaded. return True to skip it
+	return True
