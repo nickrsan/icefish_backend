c1b7337aed33ea69cadd8bee1c659576e4db2ff2
Nick <nickrsan@bitbucket.org>
2017-11-16 22:04:35 +1300

    Graphs autoupdate now

--- icefish/views.py
+++ icefish/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+import arrow
+
 from django.shortcuts import render, render_to_response
 from django.template.loader import get_template
 
@@ -11,12 +14,30 @@ from icefish.serializers import CTDSerializer
 from icefish.models import CTD
 
 
-class CTDViewSet(viewsets.ModelViewSet):
+class CTDViewSet(viewsets.ReadOnlyModelViewSet):
 	"""
 	API endpoint that allows users to be viewed or edited.
 	"""
-	queryset = CTD.objects.all().order_by('-dt')
 	serializer_class = CTDSerializer
+
+	def get_queryset(self):
+		"""
+		Optionally restricts the returned purchases to a given user,
+		by filtering against a `username` query parameter in the URL.
+		"""
+		queryset = CTD.objects.all().order_by('-dt')
+		beginning_dt = self.request.query_params.get('since', None)
+		end_dt = self.request.query_params.get('before', None)
+		if beginning_dt is not None:
+			filter_dt = datetime.datetime.strptime(beginning_dt, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=datetime.timezone.utc)
+		else:
+			filter_dt = arrow.utcnow().shift(days=-1).datetime
+		queryset = queryset.filter(dt__gt=filter_dt)
+
+		if end_dt is not None:
+			queryset = queryset.filter(dt__lt=datetime.datetime.strptime(end_dt, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=datetime.timezone.utc))
+
+		return queryset
 
 
 def spectrogram_full(request):
