3275dce9258be39162a907abb88fdc122074d4eb
Nick Santos <nickrsan@users.noreply.github.com>
2017-12-04 20:20:27 +1300

    Adding base code for controlling wiper

---
+++ icefish/static/icefish/js/controls.js
@@ -0,0 +1,36 @@
+ICEFISH_WIPER_SECONDS_PER_REVOLUTION = 9.1;  // measured by Paul and Nick
+ICEFISH_WIPER_DEGREES_PER_SECOND = 39.5604;
+ICEFISH_WIPER_MS_PER_DEGREE = 25.2777;
+ICEFISH_WIPER_BASE_URL = null; // THIS SHOULD BE DEFINED IN PAGE
+ICEFISH_WIPER_URL_ON = ICEFISH_WIPER_BASE_URL + "/Relays/Turn1On/";
+ICEFISH_WIPER_URL_OFF = ICEFISH_WIPER_BASE_URL + "/Relays/Turn1Off/";
+
+function icefish_wiper_on_off(on_off) {
+    if (on_off === "on") {
+        var url = ICEFISH_WIPER_URL_ON;
+    } else {
+        var url = ICEFISH_WIPER_URL_OFF;
+    }
+
+    $.ajax({
+        url: url,
+        success: function (data, status, xhr) {
+            console.log("Wiper turned " + on_off);
+        },
+        error: function (data, status, xhr) {
+            console.log("Wiper command failed");
+        },
+    })
+}
+
+
+
+function icefish_wiper_revolution(num_revolutions){
+    icefish_wiper_on_off("on");
+    setTimeout(icefish_wiper_on_off, ICEFISH_WIPER_SECONDS_PER_REVOLUTION * num_revolutions, "off");
+}
+
+function move_wiper(degrees){
+    icefish_wiper_on_off("on");
+    setTimeout(icefish_wiper_on_off, ICEFISH_WIPER_MS_PER_DEGREE * degrees);
+}
