99b84322a40b92c484f3902a9f3251493f9cf5cd
Nick Santos <nickrsan@users.noreply.github.com>
2017-12-04 21:36:44 +1300

    Added toggle for lights

--- icefish/static/icefish/js/controls.js
+++ icefish/static/icefish/js/controls.js
@@ -5,7 +5,7 @@ ICEFISH_WIPER_BASE_URL = ICEFISH_CONTROL_BASE_URL + ":" + ICEFISH_CONTROL_PORT;
 ICEFISH_WIPER_URL_ON = ICEFISH_WIPER_BASE_URL + "/Relays/Turn1On/";
 ICEFISH_WIPER_URL_OFF = ICEFISH_WIPER_BASE_URL + "/Relays/Turn1Off/";
 
-function icefish_wiper_on_off(on_off) {
+function icefish_wiper_toggle(on_off) {
     if (on_off === "on") {
         var url = ICEFISH_WIPER_URL_ON;
     } else {
@@ -20,17 +20,40 @@ function icefish_wiper_on_off(on_off) {
         error: function (data, status, xhr) {
             console.log("Wiper command failed");
         },
-    })
+    });
 }
 
 
 
 function icefish_wiper_revolution(num_revolutions){
-    icefish_wiper_on_off("on");
-    setTimeout(icefish_wiper_on_off, ICEFISH_WIPER_SECONDS_PER_REVOLUTION *1000 * num_revolutions, "off");
+    icefish_wiper_toggle("on");
+    setTimeout(icefish_wiper_toggle, ICEFISH_WIPER_SECONDS_PER_REVOLUTION *1000 * num_revolutions, "off");
 }
 
-function move_wiper(degrees){
-    icefish_wiper_on_off("on");
-    setTimeout(icefish_wiper_on_off, ICEFISH_WIPER_MS_PER_DEGREE * degrees, "off");
+function icefish_move_wiper(degrees){
+    icefish_wiper_toggle("on");
+    setTimeout(icefish_wiper_toggle, ICEFISH_WIPER_MS_PER_DEGREE * degrees, "off");
+}
+
+function icefish_lights_toggle(on_off){
+
+    var base_url = ICEFISH_CONTROL_BASE_URL + ":" + ICEFISH_CONTROL_PORT + "/Relays/";
+    if (on_off === "on") {
+        var urls = [base_url + "Turn2On/", base_url + "Turn4On/"]
+    } else {
+        var urls = [base_url + "Turn2Off/", base_url + "Turn4Off/"]
+    }
+
+    urls.forEach(function(url) {
+
+        $.ajax({
+            url: url,
+            success: function (data, status, xhr) {
+                console.log("Wiper turned " + on_off);
+            },
+            error: function (data, status, xhr) {
+                console.log("Wiper command failed");
+            },
+        })
+    });
 }
