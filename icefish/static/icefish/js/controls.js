6b7f73798c9c603b4c350dd260d1997d745dde2b
Nick <nickrsan@bitbucket.org>
2017-12-04 21:25:08 +1300

    Timing fixes for wiper controls

--- icefish/static/icefish/js/controls.js
+++ icefish/static/icefish/js/controls.js
@@ -1,6 +1,6 @@
-ICEFISH_WIPER_SECONDS_PER_REVOLUTION = 9.1;  // measured by Paul and Nick
-ICEFISH_WIPER_DEGREES_PER_SECOND = 39.5604;
-ICEFISH_WIPER_MS_PER_DEGREE = 25.2777;
+ICEFISH_WIPER_SECONDS_PER_REVOLUTION = 9;  // measured by Paul and Nick
+ICEFISH_WIPER_DEGREES_PER_SECOND = 40;
+ICEFISH_WIPER_MS_PER_DEGREE = 25.01088;
 ICEFISH_WIPER_BASE_URL = ICEFISH_CONTROL_BASE_URL + ":" + ICEFISH_CONTROL_PORT;
 ICEFISH_WIPER_URL_ON = ICEFISH_WIPER_BASE_URL + "/Relays/Turn1On/";
 ICEFISH_WIPER_URL_OFF = ICEFISH_WIPER_BASE_URL + "/Relays/Turn1Off/";
@@ -32,5 +32,5 @@ function icefish_wiper_revolution(num_revolutions){
 
 function move_wiper(degrees){
     icefish_wiper_on_off("on");
-    setTimeout(icefish_wiper_on_off, ICEFISH_WIPER_MS_PER_DEGREE * degrees);
+    setTimeout(icefish_wiper_on_off, ICEFISH_WIPER_MS_PER_DEGREE * degrees, "off");
 }
