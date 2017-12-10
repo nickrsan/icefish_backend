63aba2fbd84624958af383161d62377068470bf0
Nick Santos <nickrsan@users.noreply.github.com>
2017-12-10 13:55:05 +1300

    A few more potential stability fixes for video. Updates to about to include urls to people's sites, and hyperlink (or not) based on whether it's the kiosk

--- icefish/static/icefish/js/video/video_support.js
+++ icefish/static/icefish/js/video/video_support.js
@@ -1,5 +1,10 @@
 
 function recover_video(){
+    /*
+        This is an attempt to restart the stream if it fails - documentation on videojs is a little sparse on what these
+        functions actually *do*, so this is a guess, and is being tested whenever the stream fails. It's possible that the
+        tutorial docs (as opposed to the API docs) in videojs describe more how to handle this
+     */
     var video_player = videojs("video_player");
     if (video_player.ended() || video_player.paused()){  // checking this because on some errors it'll play through. Only want to reset the player if it ends
         // Try to recover the stream by starting to play again
@@ -11,6 +16,10 @@ function recover_video(){
     }
 }
 function hide_controls(){
+    /*
+        This needs more testing before it can be used again - we wanted to show controls on failure, then hide them upon
+        stream starting - we were getting an error, though maybe that was because of a resolved race condition
+     */
     if (video_player.userActive !== true){  // only hide the controls if nobody is there right now
         video_player.controls(false);
     }else {
@@ -30,5 +39,7 @@ function start_video(){
     video_player.on("abort", recover_video);
     //video_player.on("play",  hide_controls);
     //video_player.skippy({"maxErrors": 999999, "onLiveError": recover_video});
-    video_player.play();
+    video_player.ready(function(){
+        video_player.play();  // only play when ready - this is likely the issue we were having after we swapped from autoplay to this call
+    });
 }
