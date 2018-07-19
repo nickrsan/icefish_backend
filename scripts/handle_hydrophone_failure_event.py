cf364c43649a12c94f5bbd43491454ee433d1597
Nick <nickrsan@bitbucket.org>
2018-07-19 13:15:17 -0700

    Error handling for hydrophone code

--- scripts/handle_hydrophone_failure_event.py
+++ scripts/handle_hydrophone_failure_event.py
@@ -1,4 +1,10 @@
 import logging
+import os
+
+import django
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "icefish_backend.settings"
+django.setup()
 
 from icefish.data_management.hydrophone import start_array_data_manager, find_and_kill_array_data_manager
 
@@ -6,5 +12,12 @@ log = logging.getLogger("icefish.audio")
 
 if __name__ == "__main__":
 	log.warning("Hydrophone failure detected. Attempting restart")
-	find_and_kill_array_data_manager()
-	start_array_data_manager()
+	try:
+		find_and_kill_array_data_manager()
+	except:
+		log.warning("Array data manager kill was unsuccessful")
+
+	try:
+		start_array_data_manager()
+	except:
+		log.warning("Array data manager start was unsuccessful")
