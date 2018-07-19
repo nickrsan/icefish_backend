8df1b454712bcae7a5f67e76e115d7caa0867234
Nick Santos <nickrsan@users.noreply.github.com>
2018-07-19 13:50:52 -0700

    Adding email alert to paul on hydrophone failure

--- scripts/handle_hydrophone_failure_event.py
+++ scripts/handle_hydrophone_failure_event.py
@@ -2,16 +2,26 @@ import logging
 import os
 
 import django
+from django.core.mail import send_mail
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "icefish_backend.settings"
 django.setup()
 
+from icefish_backend import local_settings
 from icefish.data_management.hydrophone import start_array_data_manager, find_and_kill_array_data_manager
 
 log = logging.getLogger("icefish.audio")
 
 if __name__ == "__main__":
 	log.warning("Hydrophone failure detected. Attempting restart")
+	sent = send_mail('ADM Failed', 'Attempting to restart',
+					 local_settings.EMAIL_HOST_USER,
+					 ["pcziko@gmail.com",],
+					 fail_silently=False
+					 )
+	if sent == 0:
+		log.warning("Emails not sent to notify Paul of ADM failure")
+		
 	try:
 		find_and_kill_array_data_manager()
 	except:
